281eecd34969e0bfa8ddaa906238d6a1f06b00c3
Jan Kumor <kumor@nodalexchange.com>
2016-10-14 17:15:51 -0400

    Updated the tag for the image to include username.y

--- build_and_run.sh
+++ build_and_run.sh
@@ -7,7 +7,7 @@ HOST_DIR="/tmp/"
 # Remove unused docker images
 for x in $(docker images -q); do docker rmi $x; done
 # Build the docker image
-docker build --rm=true --tag=puppetexplorer .
+docker build --rm=true --tag=akumor/puppetexplorer .
 [ "$?" != "0" ] && echo 'ERROR: Failed to build image.' && exit 1
 # Kill and remove existing docker image of the same name
 [ "$(docker ps -a | grep -c "puppetexplorer-container")" -gt 0 ] && docker rm --force=true "puppetexplorer-container"
@@ -16,5 +16,5 @@ docker run --cpuset-cpus="0,1" \
            --memory="4g" \
            --name="puppetexplorer-container" \
            --tty=false \
-           puppetexplorer
+           akumor/puppetexplorer
                        
